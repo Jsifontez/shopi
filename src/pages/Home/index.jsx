8f87b20e82f107b27685311bcd8b2b51ef31ff93
Juan Sifontez <juan.sifontez.dev@gmail.com>
2023-05-31 11:47:28 -0400

    Add card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,12 @@
+import Card from '../../Components/Card'
+
 function Home () {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <Card />
+    </>
+  )
 }
 
 export default Home
