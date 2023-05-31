902211778e6f296a17094ca61d0e996e473cb10c
Juan Sifontez <juan.sifontez.dev@gmail.com>
2023-05-31 12:57:46 -0400

    Add list of prodcuts

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,9 +1,17 @@
-const Card = () => {
+const Card = ({ product }) => {
   return (
     <article className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
       <figure className='relative mb-2 w-full h-4/5'>
-        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>Electronics</span>
-        <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
+        <span
+          className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'
+        >
+          {product.category.name}
+        </span>
+        <img
+          className='w-full h-full object-cover rounded-lg'
+          src={product.images[0]}
+          alt={product.description}
+        />
         <span
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
         >
@@ -11,8 +19,8 @@ const Card = () => {
         </span>
       </figure>
       <p className='flex justify-between items-center'>
-        <span className='text-sm font-light'>Headphones</span>
-        <span className='text-lg font-medium'>$300</span>
+        <span className='text-sm font-light'>{product.title}</span>
+        <span className='text-lg font-medium'>${product.price}</span>
       </p>
     </article>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from 'react'
 import Card from '../../Components/Card'
 
 function Home () {
+  const [products, setProducts] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
   return (
-    <>
+    <main>
       <h1>Home</h1>
-      <Card />
-    </>
+
+      <section className='grid gap-5 grid-cols-4 w-full max-w-screen-lg'>
+        {products?.map(product => (
+          <Card key={product.id} product={product} />
+        ))}
+      </section>
+    </main>
   )
 }
 
