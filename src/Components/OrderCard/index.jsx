aad33cf49a8557782ae71340d30fe5102d63f8b6
Juan Sifontez <juan.sifontez.dev@gmail.com>
2023-06-11 23:31:30 -0400

    Add quantity measure of checkout items

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,20 +1,31 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { TrashIcon, PlusIcon, MinusIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = ({ id, title, imageUrl, price, quantity, handleDelete }) => {
   return (
     <li className='flex justify-between items-center mb-3'>
       <div className='flex items-center gap-2'>
-        <span className='w-3'>{quantity}</span>
         <figure className='w-20 h-20 m-0'>
           <img className='h-full w-full rounded-lg object-cover' src={imageUrl} alt={title} />
         </figure>
-        <p className='text-sm font-light'>{title}</p>
+        <div className='flex flex-col'>
+          <p className='text-lg font-medium'>${price}</p>
+          <p className='text-sm font-light'>{title}</p>
+        </div>
       </div>
 
       <div className='flex items-center gap-2'>
-        <p className='text-lg font-medium'>${price}</p>
+        <button>
+          <MinusIcon className="h-3 w-3 text-black" />
+        </button>
+
+        <span className='w-4 bg-gray-300 text-sm text-center'>{quantity}</span>
+
+        <button>
+          <PlusIcon className="h-3 w-3 text-black" />
+        </button>
+
         <button onClick={() => handleDelete(id)}>
-          <XMarkIcon className="h-6 w-6 text-black" />
+          <TrashIcon className="h-4 w-4 text-black" />
         </button>
       </div>
     </li>
