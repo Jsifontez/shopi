2e3953e7da4d3f304907fc5af6acc38e1ed8e31f
Juan Sifontez <juan.sifontez.dev@gmail.com>
2023-06-15 10:59:35 -0400

    Add my orders views with cards

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,15 +1,23 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 import OrderCard from '../../Components/OrderCard'
 import { ShoppingCartContext } from '../../context'
 
 function MyOrder () {
-  const { order } = useContext(ShoppingCartContext)
+  const { orders } = useContext(ShoppingCartContext)
   return (
     <>
-      <h1>MyOrder</h1>
+      <div className='flex items-center justify-center w-80 relative'>
+        <Link to='/my-orders' className='absolute left-0'>
+          <ChevronLeftIcon className='h-6 w-6 text-black' />
+        </Link>
+        <h1>MyOrders</h1>
+      </div>
+
       <section className='mt-5'>
         <ul>
-          {order?.slice(-1)[0].products.map(product => (
+          {orders?.slice(-1)[0].products.map(product => (
             <OrderCard
               key={product.id}
               id={product.id}
--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,5 +1,25 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+
+import { ShoppingCartContext } from '../../context'
+import OrdersCard from '../../Components/OrdersCard'
+
 function MyOrders () {
-  return <h1>MyOrders</h1>
+  const { orders } = useContext(ShoppingCartContext)
+  return (
+    <>
+      <h1>MyOrders</h1>
+
+      {orders.map((order, index) => (
+        <Link key={index} to={`/my-orders/${order.id}`}>
+          <OrdersCard
+            totalPrice={order.totalPrice}
+            totalProducts={order.totalProducts}
+          />
+        </Link>
+      ))}
+    </>
+  )
 }
 
 export default MyOrders
