6bcfe0a588cd3edd1b4a853e7836fb8652c52aa6
Juan Sifontez <juan.sifontez.dev@gmail.com>
2023-07-18 00:59:36 -0400

    Add toggle checkoutSideMenu from navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,7 +7,12 @@ import './styles.css'
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false)
-  const { count } = useContext(ShoppingCartContext)
+  const {
+    count,
+    isCheckoutSideMenuOpen,
+    openCheckoutSideMenu,
+    closeCheckoutSideMenu
+  } = useContext(ShoppingCartContext)
 
   const leftLinks = [
     {
@@ -75,6 +80,11 @@ const Navbar = () => {
     }
   ]
 
+  const handleCheckoutSideMenu = () => {
+    if (isCheckoutSideMenuOpen) closeCheckoutSideMenu()
+    else openCheckoutSideMenu()
+  }
+
   return (
     <nav className='fixed z-10 w-full py-5 px-8 top-0'>
       <div className='flex justify-between items-center w-full navbar--desktop'>
@@ -93,8 +103,8 @@ const Navbar = () => {
             <NavItem key={link.text} link={link} />
           ))}
 
-          <li className='flex items-center'>
-            <ShoppingBagIcon className='h-6 w-6 text-black'/>
+          <li className='flex items-center cursor-pointer' onClick={handleCheckoutSideMenu}>
+            <ShoppingBagIcon className='h-6 w-6 text-black' />
             <span>{count}</span>
           </li>
         </ul>
@@ -127,8 +137,11 @@ const Navbar = () => {
             </ul>
           </div>
         )}
-        <div className='flex items-center fixed top-5 right-4 bg-white p-2 rounded-full'>
-          <ShoppingBagIcon className='h-6 w-6 text-black'/>
+        <div
+          className='flex items-center fixed top-5 right-4 bg-white p-2 rounded-full cursor-pointer'
+          onClick={handleCheckoutSideMenu}
+        >
+          <ShoppingBagIcon className='h-6 w-6 text-black' />
           <span>{count}</span>
         </div>
       </div>
